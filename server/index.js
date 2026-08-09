4a2240e11173c7fb136116a7fb302ec443e18c79
shirishstha <sthasiriz123@gmail.com>
2026-08-09 21:12:37 +0545

    demo of params ,route and query params

--- server/index.js
+++ server/index.js
@@ -4,9 +4,38 @@ const app = express();
 
 const PORT = 8080;
 
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello world');
 })
+
+app.get('/movie/:id', (req, res) => {
+    res.send(req.params.name);
+})
+app.get('/movie', (req, res) => {
+    res.json({
+        success: true,
+        data: {
+            name: req.query.name
+        }
+    });
+})
+
+app.post('/movie/create',(req,res)=>{
+    const data = req.body;
+    //code to add movie in data base
+    
+    res.json({
+        success:true,
+        message:"Movie created successfully"
+    })
+
+})
+
+
+
 
 
 app.listen(PORT, () => {
